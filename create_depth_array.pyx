d0bb5ca417b861d8a19dbbc04de2a5209419c98b
Brad Nelson <bnelsj@gmail.com>
2017-08-25 10:13:44 -0700

    Added ctypes for index variables

--- create_depth_array.pyx
+++ create_depth_array.pyx
@@ -10,7 +10,7 @@ ctypedef np.uint16_t DTYPE_t
 @cython.wraparound(False)
 
 def create_depth_array(unsigned short [:, :] count, unsigned short [:, :] depth, unsigned int rlen=36):
-
+    cdef unsigned int i, j, k
     for i in range(count.shape[0] - rlen):
         for j in range(count.shape[1]):
             if count[i, j] != 0:
